a97f2a37fc98c0b1552d38153eba6b99e6b3c691
Peter Njuguna <njugunapm23@gmail.com>
2022-11-10 20:15:05 +0300

    added and designed the contact us section

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React from "react";
+
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: "3rem" }}>
+        Find Us
+      </h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">Jericho ACK, Rabai Road CHG2018</p>
+        <p
+          className="p__cormorant"
+          style={{ color: "#DCCA87", margin: "2rem 0" }}
+        >
+          Opening Hours
+        </p>
+        <p className="p__opensans">Weekdays - 8:00 am - 7:00 pm</p>
+        <p className="p__opensans">Weekends - 9:00 am - 3:00 pm</p>
+      </div>
+      <button className="custom__button" style={{ marginTop: "2rem" }}>
+        Visit Us
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 );
 
